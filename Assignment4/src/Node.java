cfad6456ade2eb565f0ab868542a93d55392583d
Daniel Hug <dhug@albany.edu>
2016-10-30 17:15:03 -0400

    Implemented the properties value and next in the Node class, and made a constructor

--- Assignment4/src/Node.java
+++ Assignment4/src/Node.java
@@ -1,4 +1,21 @@
+/*
+ * Daniel Hug
+ * Assignment 4 
+ * ICSI 213
+ */
 
+/**
+ * The Node class is used to implement the
+ *  linked list.
+ * 
+ *
+ */
 public class Node {
+	String value;
+	Node next;
+	Node(String val, Node n){
+		value = val;
+		next = n;
+	}
 
 }
